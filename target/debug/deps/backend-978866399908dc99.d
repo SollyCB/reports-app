220f69ad48598054241d246715e59f5b06794e35
sozomon <sozzzy299@gmail.com>
2023-02-02 06:58:47 +0000

    added gitignore

---
+++ target/debug/deps/backend-978866399908dc99.d
@@ -0,0 +1,5 @@
+/home/solly/code/rust/reports-app/target/debug/deps/backend-978866399908dc99.rmeta: backend/src/main.rs
+
+/home/solly/code/rust/reports-app/target/debug/deps/backend-978866399908dc99.d: backend/src/main.rs
+
+backend/src/main.rs:
